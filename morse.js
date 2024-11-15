36c5658db12dc327d95be9fd7ba6c3861ec3dd08
nonanebis <nonanebis@mxlol>
2024-11-15 14:56:11 +0100

    first commit

---
+++ morse.js
@@ -0,0 +1,15 @@
+
+
+
+
+let getLatinCharacterList  = [
+	'A','.-',
+	'B','-...',
+	'C','-.-.',
+	'D','-..',
+	'E','.',
+	'F','..-.',
+];
+getLatinCharacterList.forEach((element) => console.log(getLatinCharacterList));
+for (let i = 'A'; i < 'F'; i++){
+    console.log(i);}
